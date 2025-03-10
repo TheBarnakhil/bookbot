f5f7c9ac6793da346c82f21ccfd0278d455faac1
TheBarnakhil <nsakhil763@gmail.com>
2025-03-11 00:27:16 +0530

    Added sys.argv to accept book name from terminal

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import sys
+
 from stats import get_book_text , get_letter_count, pretty_print_dict
 
 def main() :
-    path_to_book = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    path_to_book = sys.argv[1]
 
     file_content = get_book_text(path_to_book)
     letter_count = get_letter_count(file_content)
